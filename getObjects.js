f26cc26dea8994df7bf648cdf78b5018cd46b667
Joel Plowright <joelplow@gmail.com>
2018-11-27 22:52:39 +0000

    add getObjects.js

---
+++ getObjects.js
@@ -0,0 +1,28 @@
+function getObjects(str, i0) {
+  i0 = i0 || 0
+
+  var objs = []
+
+  for(var i=i0; i<str.length; i=skipWhitespace(str, i+obj.length)) {
+    var obj = getObject(str, i)
+    if(obj)
+      objs.push(obj)
+    else if(i==i0)
+      return null
+    else
+      return {
+        objects: objs,
+        "length": i-i0,
+      }
+  }
+  if(i!=i0)
+    return {
+      objects: objs,
+      "length": i-i0,
+    }
+
+}
+
+module.exports = getObjects
+const getObject = require("./getObject")
+const skipWhitespace = require("./skipWhitespace")
